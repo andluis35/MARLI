af496cd3be75cd4849a59f4136b4228aff07b927
andluis35 <and.luis35@gmail.com>
2026-06-27 12:53:05 -0300

    Adicionada refatoração no cruzamento das bases

--- src/cruzamento_bases.py
+++ src/cruzamento_bases.py
@@ -1,75 +1,58 @@
 import pandas as pd
-from pathlib import Path
 from time import sleep
 
 
-# Definição de caminhos absolutos e DataFrames
-BASE_DIR = Path(__file__).resolve().parent.parent
-CLEAN_DIR = BASE_DIR / "data" / "clean"
-BASE_EDITAIS = CLEAN_DIR / "clean_base_editais.xlsx"
-BASE_HOMOLOGACOES = CLEAN_DIR / "clean_base_homologacoes.xlsx"
-BASE_LICITACOES = CLEAN_DIR / "clean_base_licitacoes.xlsx"
-BASE_PARTICIPANTES = CLEAN_DIR / "clean_base_participantes.xlsx"
-
-df_editais = pd.read_excel(BASE_EDITAIS)
-df_homologacoes = pd.read_excel(BASE_HOMOLOGACOES)
-df_licitacoes = pd.read_excel(BASE_LICITACOES)
-df_participantes = pd.read_excel(BASE_PARTICIPANTES)
-df_master = df_licitacoes.copy()
-qtd_linhas_originais = df_master.shape[0]
-
-
-def merge_homologacao(df):
+def merge_homologacao(df_m, df_h, qtd_linhas_originais):
     """
-        Cruza os dados da base de Licitações com a base de Homologações
+        Cruza os dados da base Master com a base de Homologações.
     """
 
     # Agrupa as licitações e soma o valor total de cada uma delas, já que uma
-    # licitação pode ter vários itens homologados (várias linhas)
-    df_homologacoes_agg = df_homologacoes.groupby('licitacao', as_index=False)['valor_homologacao'].sum()
+    # licitação pode ter vários itens homologados (várias linhas).
+    df_homologacoes_agg = df_h.groupby('licitacao', as_index=False)['valor_homologacao'].sum()
 
     # Efetua o merge das bases em questão, usando a coluna 'licitacao' como chave.
-    # how='left' adiciona as novas colunas de 'df_homologacoes_agg' à direita de 'df'.
-    # Se uma licitação do 'df' não existir na tabela de homologações, preenche com nulo.
-    df = pd.merge(df, df_homologacoes_agg, on='licitacao', how='left')
+    # how='left' adiciona as novas colunas de 'df_homologacoes_agg' à direita de 'df_m'.
+    # Se uma licitação do 'df_m' não existir na tabela de homologações, preenche com nulo.
+    df_m = pd.merge(df_m, df_homologacoes_agg, on='licitacao', how='left')
 
-    # O teste booleano 'df.shape[0] == qtd_linhas_originais' verifica se não houve duplicações indevidas.
-    print(f"[MERGE 1] Homologação acoplada. Linhas mantidas: {df.shape[0] == qtd_linhas_originais}")
+    # O teste booleano 'df_m.shape[0] == qtd_linhas_originais' verifica se não houve duplicações indevidas.
+    print(f"[MERGE 1] Homologação acoplada. Linhas mantidas: {df_m.shape[0] == qtd_linhas_originais}")
 
-    return df
+    return df_m
 
 
-def merge_editais(df):
+def merge_editais(df_m, df_e, qtd_linhas_originais):
     """
-        Cruza os dados da base de Licitações com a base de Editais
+        Cruza os dados da base Master com a base de Editais.
     """
 
-    # Remove duplicatas da base de editais
-    df_editais_unique = df_editais.drop_duplicates(subset=['numeroedital'])
+    # Remove duplicatas da base de editais.
+    df_editais_unique = df_e.drop_duplicates(subset=['numeroedital'])
 
     # Efetua o merge das bases em questão.
-    # left_on='numero_edital' indica que 'numero_edital' do 'df' será a chave de ligação.
+    # left_on='numero_edital' indica que 'numero_edital' do 'df_m' será a chave de ligação.
     # right_on='numeroedital' indica que a coluna correspondente do 'df_editais_unique' se chama 'numeroedital'.
-    # how='left' adiciona as novas colunas de 'df_editais_unique' à direita de 'df'.
-    df = pd.merge(
-        df,
+    # how='left' adiciona as novas colunas de 'df_editais_unique' à direita de 'df_m'.
+    df_m = pd.merge(
+        df_m,
         df_editais_unique,
         left_on='numero_edital',
         right_on='numeroedital',
         how='left'
     )
 
-    # Remove a coluna duplicada do número do edital gerada pelo cruzamento
-    if 'numeroedital' in df.columns:
-        df = df.drop(columns=['numeroedital'])
+    # Remove a coluna duplicada do número do edital gerada pelo cruzamento.
+    if 'numeroedital' in df_m.columns:
+        df_m = df_m.drop(columns=['numeroedital'])
 
-    # O teste booleano 'df.shape[0] == qtd_linhas_originais' verifica se não houve duplicações indevidas.
-    print(f"[MERGE 2] Editais acoplados. Linhas mantidas: {df.shape[0] == qtd_linhas_originais}")
+    # O teste booleano 'df_m.shape[0] == qtd_linhas_originais' verifica se não houve duplicações indevidas.
+    print(f"[MERGE 2] Editais acoplados. Linhas mantidas: {df_m.shape[0] == qtd_linhas_originais}")
 
-    return df
+    return df_m
 
 
-def merge_participantes(df):
+def merge_participantes(df_m, df_p):
     """
         Cruza os dados da base de Licitações com a base de Participantes
     """
@@ -80,24 +63,32 @@ def merge_participantes(df):
     # O merge não será feito para não multiplicar o número de linhas do 'df_master'.
     print(f"[STATUS] Base de Participantes reservada na memória para os cálculos heurísticos.")
 
-    return df
+    return df_m
 
 
-if __name__ == "__main__":
+def executar_cruzamento(df_editais, df_homologacoes, df_licitacoes, df_participantes):
+    """
+        Realiza o cruzamento das bases já limpas.
+    """
+    
     print("\n" * 50)
     print("-" * 50)
     print("INICIANDO FASE 2: CRUZAMENTO DAS BASES")
     print("-" * 50)
 
-    df_master = merge_homologacao(df_master)
-    df_master = merge_editais(df_master)
-    df_master = merge_participantes(df_master)
-    df_master.to_excel(CLEAN_DIR / "clean_base_master.xlsx", index=False)
-    print("[OK] Bases acopladas e salvas com sucesso!")
+    df_master = df_licitacoes.copy()
+    qtd_linhas_originais = df_master.shape[0]
+
+    df_master = merge_homologacao(df_m=df_master, df_h=df_homologacoes, qtd_linhas_originais=qtd_linhas_originais)
+    df_master = merge_editais(df_m=df_master, df_e=df_editais, qtd_linhas_originais=qtd_linhas_originais)
+    df_master = merge_participantes(df_m=df_master, df_p=df_participantes)
+    print("[OK] Bases acopladas com sucesso!")
 
     print("-" * 50)
     print("== RESUMO DO df_master APÓS CRUZAMENTOS ==\n")
     print(df_master.info())
     print("-" * 50)
     sleep(2)
+
+    return df_master
     
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from cruzamento_bases import executar_cruzamento
 from etl_dados import executar_etl
 from pathlib import Path
 from time import sleep
@@ -18,4 +19,7 @@ if __name__ == "__main__":
 
     # FASE 1: Extração e Limpeza
     df_editais, df_homologacoes, df_licitacoes, df_participantes = executar_etl(RAW_DIR)
+
+    # FASE 2: Cruzamento dos Dados
+    df_master = executar_cruzamento(df_editais, df_homologacoes, df_licitacoes, df_participantes)
     
